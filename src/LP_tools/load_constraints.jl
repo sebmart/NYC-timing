b427dfcf15f8579447011d77c480ff54b80c8126
Arthur Delarue <adelarue@mit.edu>
2015-07-02 17:17:15 -0400

    Main initial commit. This transfers most of the analysis code to this repo.
    This is not guaranteed to be bug-free. All of the cogs of the machine still need to be meshed together properly.

---
+++ src/LP_tools/load_constraints.jl
@@ -0,0 +1,98 @@
+# load_constraints.jl
+# Gets city information from manhattan.jld object and travel time information from Travel time CSV
+# Authored by Arthur J Delarue on 6/8/15
+
+function saveManhattan(pb::TaxiProblem, name::String; compress=false)
+  save("Cities/Saved/$name.jld", "pb", pb, compress=compress)
+end
+
+function loadManhattan(name::String)
+  pb = load("Cities/Saved/$name.jld","pb")
+  return pb
+end
+
+function saveRoadTimes(times::SparseMatrixCSC{Float64,Int}, name::String; compress=false)
+  save("Outputs/$name.jld", "times", times, compress=compress)
+end
+
+function loadRoadTimes(name::String)
+  times = load("Outputs/$name.jld","times")
+  return times
+end
+
+MANHATTAN_NODES = 6134
+# Speed limits in km/h from OpenStreetMap/src/speeds.jl
+URBAN_SPEEDS = [95,72,48,32,22,12,8,5]
+
+function loadCityGraph(;fromScratch::Bool=false, useShortestPaths::Bool=true,fixSpeeds=false)
+	"""
+	Load Manhattan object and return it
+	"""
+	println("**** Loading city graph ****")
+	if fromScratch
+		manhattan = Manhattan(sp=useShortestPaths)
+		# if useShortestPaths
+		# 	sp = manhattan.sp
+		# else
+		# 	sp = parallelShortestPathsAuto(manhattan.network, manhattan.roadTime, manhattan.roadCost)
+		# 	manhattan.sp = sp
+		# end
+		if fixSpeeds
+			# Make sure the speeds are in URBAN_SPEEDS
+			# If not match speed to closest actual speed
+			for i=1:MANHATTAN_NODES,j=1:MANHATTAN_NODES
+				if manhattan.distances[i,j] != 0
+					# Don't forget to convert from m/s to km/h
+					if manhattan.roadTime[i,j] != 0
+						speed = 3.6 * manhattan.distances[i,j]/manhattan.roadTime[i,j]
+					else
+						speed = 3.6 * manhattan.distances[i,j]/0.1
+					end
+					road_type = indmin(abs(URBAN_SPEEDS - speed))
+					if manhattan.roadTime[i,j] == 0
+						println(manhattan.roadTime[i,j], "\t", manhattan.distances[i,j], "\t", speed, "\t", URBAN_SPEEDS[road_type])
+					end
+					manhattan.roadTime[i,j] = manhattan.distances[i,j]/URBAN_SPEEDS[road_type]
+				end
+			end
+			shortestPaths!(manhattan)
+		end
+		saveManhattan(manhattan, "full_manhattan")
+	else
+		manhattan = loadManhattan("full_manhattan")
+	end
+	return manhattan
+end
+
+function loadTravelTimeData(;radius::Int=40, times::String= "1214", min_rides::Int=4, num_nodes::Int=MANHATTAN_NODES, preprocess::Bool=false, num_clusters::Int64 = 50, sampleSize::Int64=10000) 
+	# Hardcoded for simplicity. Map of Manhattan not likely to change anytime soon
+	"""
+	Return array of travel times between pairs of nodes A,B obtained from NYC dataset
+	"""
+	println("**** Loading travel times ****")
+	# Load Manhattan highway nodes
+	highwayNodes = load("Cities/Saved/highwayNodes.jld", "highwayNodes")
+	if preprocess
+		dataFile = "Travel_times/travel_times_r$(radius)_wd_$(times)_clust$(num_clusters)_rides$(sampleSize)_minr$(min_rides).csv"
+	else
+		dataFile = "Travel_times/travel_times_r$(radius)_wd_$(times).csv"
+	end
+	println("-- Loading from $dataFile...")
+	data = readcsv(dataFile)
+	nodePairs = data[:,1]
+	average = data[:,2]
+	minRides = data[:,5]
+
+	# Put travel time data in an array
+	travelTimes = zeros(num_nodes,num_nodes)
+	for (i, element) in enumerate(nodePairs)
+		node = split(element, ";")
+		src = int(node[1]) + 1
+		dest = int(node[2]) + 1
+		if minRides[i] >= min_rides && src != dest && !(src in highwayNodes) && !(dest in highwayNodes)
+			travelTimes[src, dest] = average[i]
+			# Remember +1 because Python indexes from 0 but Julia from 1
+		end
+	end
+	return travelTimes
+end
---
+++ src/LP_tools/shortestpath.jl
@@ -0,0 +1,111 @@
+#Run an all-pair shortest path using dijkstra, minimizing time and not costs
+function shortestPaths!(pb::TaxiProblem)
+    pb.sp = shortestPaths(pb.network, pb.roadTime, pb.roadCost)
+end
+
+
+function shortestPaths(n::Network, roadTime::SparseMatrixCSC{Float64, Int},
+                                   roadCost::SparseMatrixCSC{Float64, Int})
+
+  nLocs  = length( vertices(n))
+  pathTime = Array(Float64, (nLocs,nLocs))
+  pathCost = Array(Float64, (nLocs,nLocs))
+  previous = Array(Int, (nLocs,nLocs))
+
+  for i in 1:nLocs
+    parents, dists, costs = custom_dijkstra(n,i, roadTime, roadCost)
+    previous[i,:] = parents
+    pathTime[i,:] = dists
+    pathCost[i,:] = costs
+  end
+  return ShortPaths(pathTime, pathCost, previous)
+end
+
+#Compute the table of the next locations on the shortest paths
+#next[i, j] = location after i when going to j
+function nextLoc(n::Network, sp::ShortPaths, roadTime::SparseMatrixCSC{Float64, Int})
+  nLocs = size(sp.previous,1)
+  next = Array(Int, (nLocs,nLocs))
+  for i in 1:nLocs, j in 1:nLocs
+    if i == j
+      next[i,i] = i
+    else
+      minTime = Inf
+      mink = 0
+      for n in out_neighbors(n,i)
+        if roadTime(i,n) + sp.traveltime[n,j] < minTime
+          mink = n
+          minTime = roadTime(i,n) + sp.traveltime[n,j]
+        end
+      end
+      next[i,j] = mink
+    end
+  end
+  return next
+end
+
+# TEMPORARY FIX
+# Dijkstra algorithm
+
+function custom_dijkstra(
+    g::AbstractGraph,
+    src::Int,
+    edge_dists::AbstractArray{Float64, 2},
+    edge_costs::AbstractArray{Float64, 2})
+
+    # find number of vertices
+    nvg = nv(g)
+    # initialize return types
+    dists = fill(typemax(Float64), nvg)
+    costs = fill(typemax(Float64), nvg)
+    parents = zeros(Int, nvg)
+    visited = falses(nvg)
+    # Create mutable binary heap and associated hashmap
+    h = DijkstraEntry{Float64}[]
+    sizehint(h, nvg)
+    H = mutable_binary_minheap(h)
+    hmap = zeros(Int, nvg)
+    dists[src] = 0.0
+    costs[src] = 0.0
+    # Add source node to heap
+    ref = push!(H, DijkstraEntry{Float64}(src, dists[src], costs[src]))
+    hmap[src] = ref
+    # As long as all edges have not been explored
+    while !isempty(H)
+        # Retrieve closest element to source
+        hentry = pop!(H)
+        u = hentry.vertex
+        # Look at all its neighbors and update relevant distances if necessary
+        for v in out_neighbors(g,u)
+            if dists[u] == typemax(Float64)
+                alt = typemax(Float64)
+                alt2 = typemax(Float64)
+            else
+                alt = dists[u] + edge_dists[u,v]
+                alt2 = costs[u] + edge_costs[u,v]
+            end
+            if !visited[v]  # If a vertex has never been visited, push it to the heap
+                dists[v] = alt
+                costs[v] = alt2
+                parents[v] = u
+                visited[v] = true
+                ref = push!(H, DijkstraEntry{Float64}(v, alt, alt2))
+                hmap[v] = ref
+            else            # If a vertex has been visited, decrease its key if distance estimate decreases
+                if alt < dists[v]
+                    dists[v] = alt
+                    costs[v] = alt2
+                    parents[v] = u
+                    update!(H, hmap[v], DijkstraEntry{Float64}(v, alt, alt2))
+                end
+            end
+        end
+    end
+
+    dists[src] = 0
+    costs[src] = 0.0
+    parents[src] = 0
+
+
+    return parents, dists, costs
+end
