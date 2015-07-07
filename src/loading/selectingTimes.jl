3a8603851bb3e738e3fcfaad66f0591930373a99
Brandon <bzeng@mit.edu>
2015-07-07 11:29:22 -0400

    selectingTimes up and running

--- src/loading/selectingTimes.jl
+++ src/loading/selectingTimes.jl
@@ -1,8 +1,9 @@
-using HDF5, JLD, KDTrees, DataFrames
+using HDF5, JLD, KDTrees, DataFrames, Base.Dates
 
 function selectingTimes (start::Int64, finish::Int64, radius::Float64)
 	cd("/Users/bzeng/Dropbox (MIT)/7 Coding/UROP/Data")
-
+	start = 12
+	finish = 14
 	startingHour = start
 	endingHour = finish
 	radius = 140.0
@@ -16,26 +17,26 @@ function selectingTimes (start::Int64, finish::Int64, radius::Float64)
 	    nodes[2,i] = p[2]
 	end
 
-	
 	tree = KDTree(nodes)
-	# pnode = Int[knn(tree,[px[i],py[i]],1)[1][1] for i in 1:length(px)]
-	# dnode = Int[knn(tree,[dx[i],dy[i]],1)[1][1] for i in 1:length(dx)]df
 
-	function coordinatesToNode(x::Float32, y::Float32)
+	function coordinatesToNode(x::Float64, y::Float64)
 		return inball(tree, [x, y], radius, false)
 	end
 
 	for i = 1:12
 		println(i)
 		df = 0
-		df = readtable("reduced_trip_data_$(i).csv");
+		@time df = readtable("reduced_trip_data_$(i).csv");
 		for j = 1:nrow(df)
+			if j % 1000 == 0
+				print("\r $(j * 100 / nrow(df))                   ")
+			end
 			pickup = df[j, :pickup_datetime]
 			dropoff = df[j, :dropoff_datetime]
 			pickupTime = DateTime(pickup, "y-m-d H:M:S")
 			dropoffTime = DateTime(dropoff, "y-m-d H:M:S")
-			pickupFlag = 1 <= dayofweek(pickupTime) <= 5 && startingHour <= Hour(pickupTime) <= endingHour
-			dropoffFlag = 1 <= dayofweek(dropoffTime) <= 5 && startingHour <= Hour(dropoffTime) <= endingHour
+			pickupFlag = 1 <= dayofweek(pickupTime) <= 5 && startingHour <= hour(pickupTime) <= endingHour
+			dropoffFlag = 1 <= dayofweek(dropoffTime) <= 5 && startingHour <= hour(dropoffTime) <= endingHour
 			if pickupFlag && dropoffFlag
 				nodeSrcList = coordinatesToNode(df[j, :pickup_x], df[j, :pickup_y])
 				nodeDstList = coordinatesToNode(df[j, :dropoff_x], df[j, :dropoff_y])
@@ -56,7 +57,7 @@ function selectingTimes (start::Int64, finish::Int64, radius::Float64)
 		end
 	end
 
-	writetable("trip_data_$(startingHour)_$(endingHour).csv", df)
+	@time writetable("trip_data_$(startingHour)_$(endingHour).csv", df)
 end
 
 @time selectingTimes(12, 14, 140.0)
