7357afcc8fad7688ac75bf5cc16719e380df1a39
Sebastien Martin <semartin@equityhead.msm.local>
2015-11-09 11:31:11 -0500

    Updated small changes to maintain git repo on Sloan computing grid

--- src/script.sh
+++ src/script.sh
@@ -5,10 +5,11 @@
 for fileName in `cat JSONparams/paramList.txt`
 do
 	JSONfileName="JSONparams/"$fileName
-	outputFileName="out_"${fileName%.json}".log"
+	outputFileName="Log_files/out_"${fileName%.json}".log"
 	if [ -f $FILE ];
 	then
 		rm $outputFileName
 	fi
 	qsub -pe mthread 10 -l mem_free=40G -o $outputFileName ./juliaCall.sh $JSONfileName
 done
+
